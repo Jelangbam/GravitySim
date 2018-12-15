de137ef406b57c9c37e0a1d2660eea0d70f381fb
Jason Elangbam <marthmadness@gmail.com>
2018-12-15 18:09:02 -0500

    Update forces.h
    * Forgot to add this to last commit...

--- include/forces.h
+++ include/forces.h
@@ -3,8 +3,8 @@
 #include <array>
 #include "particle.h"
 namespace forces {
-	double magnitude(const std::array<double, 3> vector);
-	std::array<double, 3> rsquared(const Particle a, const Particle b);
+	double magnitude(const std::array<double, 3>& vector);
+	std::array<double, 3> rsquared(const Particle& a, const Particle& b);
 }
 
 #endif
